fba0af8e205deca435f8c52cb7c68e735329a92b
KrzysztofPogorzelski <krzysztof.pogorzelski@autoprotectgroup.co.uk>
2023-09-17 05:04:18 +0100

    [Dashboard] - Adding some basic navbar to account dashboard

---
+++ src/components/DashboardNavbar.tsx
@@ -0,0 +1,9 @@
+import UserNav  from './UserNav'
+
+const DashboardNavbar = () => {
+  return (
+    <div className='w-full border-b h-12 flex items-center px-4 justify-end'><UserNav /></div>
+  )
+}
+
+export default DashboardNavbar
