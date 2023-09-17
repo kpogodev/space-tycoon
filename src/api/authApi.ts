208acef9c010f3b2da2c15b7ee2f26bb28e334a5
KrzysztofPogorzelski <krzysztof.pogorzelski@autoprotectgroup.co.uk>
2023-09-17 21:17:18 +0100

    [Auth] - Small costmetic change in authApi

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -2,11 +2,11 @@ import { axiosInstanceInternal } from '@/config/axiosConfig'
 import { AxiosResponse } from 'axios'
 import type { LoginInputs, RegisterInputs, AuthResponseData } from '@/types/auth'
 
+
 type ResponseData<T> = {
     success: boolean
     data: T
 }
-
 type ResponseType<T> = Promise<AxiosResponse<ResponseData<T>>>
 
 type CheckResponseType = ResponseType<undefined>
@@ -14,9 +14,16 @@ type RegisterResponseType = ResponseType<AuthResponseData>
 type LoginResponseType = ResponseType<AuthResponseData>
 type LogoutResponseType = ResponseType<undefined>
 
+const requestUrl = {
+    check: '/auth/check',
+    register: '/auth/register',
+    login: '/auth/login',
+    logout: '/auth/logout',
+}
+
 export default {
-    check: (): CheckResponseType => axiosInstanceInternal.post('/auth/check'),
-    register: (payload: RegisterInputs): RegisterResponseType => axiosInstanceInternal.post('/auth/register', payload),
-    login: (payload: LoginInputs): LoginResponseType => axiosInstanceInternal.post('/auth/login', payload),
-    logout: (): LogoutResponseType => axiosInstanceInternal.post('/auth/logout'),
+    check: (): CheckResponseType => axiosInstanceInternal.post(requestUrl.check),
+    register: (payload: RegisterInputs): RegisterResponseType => axiosInstanceInternal.post(requestUrl.register, payload),
+    login: (payload: LoginInputs): LoginResponseType => axiosInstanceInternal.post(requestUrl.login, payload),
+    logout: (): LogoutResponseType => axiosInstanceInternal.post(requestUrl.logout),
 }
